13a41daf276c9236ee48869a16858888617442a8
MoeLeak <i@leak.moe>
2025-12-06 22:06:40 +0800

    fix: Missing openssl in Windows

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
         {
           default = pkgs.stdenv.mkDerivation rec {
             pname = "connecttool-qt";
-            version = "1.4.8";
+            version = "1.4.9";
 
             # Keep entire working tree (including untracked) so new sources are present.
             src = ./.;
